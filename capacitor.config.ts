2535bd0685a4a3dfd1959fd8649d07bbd380103a
he03villa <he03villa@hotmail.com>
2025-02-01 16:42:12 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front_handy_receipt',
+  webDir: 'www'
+};
+
+export default config;
